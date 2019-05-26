7ea189435a115107c2725db79df2b43b3092373f
Dave Kane <davelkane@gmail.com>
2019-05-26 00:00:07 -0500

    Assignment complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,73 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function plusTwo(val) {
+  let num = 2;
+  val;
+  let addsToNum = () => {
+    return num + val;
+  };
+  return addsToNum();
+}
+
+console.log(plusTwo(4));
+
+let thisNum = plusTwo(35);
+
+console.log(thisNum)
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+const counter = (() => {
+  let count = 0;
+  return () => { count += 1; return count }
+})();
+
+const newCounter = counter;
+
+console.log(newCounter());
+newCounter();
+newCounter();
+newCounter();
+newCounter();
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+// const counterFactory = (() => {
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+const counterFactory = (() => {
+  let count = 0;
+  const changeBy = (val) => {
+    count += val;
+  }
+  return {
+    increment: () => {
+      changeBy(1);
+      return count;
+    },
+    decrement: () => {
+      changeBy(-1);
+      return count;
+    },
+  };   
+})();
+
+counterFactory.increment();
+counterFactory.increment();
+counterFactory.increment();
+counterFactory.increment();
+counterFactory.increment();
+console.log(counterFactory.increment());
+
+counterFactory.decrement();
+counterFactory.decrement();
+counterFactory.decrement();
+console.log(counterFactory.decrement());
